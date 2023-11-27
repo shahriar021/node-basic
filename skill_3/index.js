3333467d9ec2fa5ee650ad8811c533f3b37a46f2
shahriar021 <shahriarc19@gmail.com>
2023-11-27 18:55:24 +0600

    os path

---
+++ skill_3/index.js
@@ -0,0 +1,21 @@
+//os,path
+
+const os = require("os");
+
+// console.log(os.totalmem());
+// console.log(os.freemem());
+// console.log(os.loadavg());
+
+// console.log(os)
+
+// console.log(__dirname)
+// console.log(__filename)
+
+const path = require("path");
+
+// const extensionName = path.extname("index.html");
+// console.log(extensionName);
+
+
+const join = path.join(__dirname+"/../views")
+console.log(join)
