b0a4cae0a1bf2aca9f17360f08135c9b6f20d82a
shahriar021 <shahriarc19@gmail.com>
2023-11-28 07:05:39 +0600

    http server

--- skill_4/server.js
+++ skill_4/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const myserver = http.createServer((req, res) => {
-  res.end("hey i'm from server....delete me");
+  res.end("hey i'm from server....");
 });
 
 myserver.listen(5000, () => {
