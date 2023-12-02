137a2e60b97e052fd2247353792fa0114d2f28a3
shahriar021 <shahriarc19@gmail.com>
2023-12-02 18:27:04 +0600

    cookis status

--- exp_1/index.js
+++ exp_1/index.js
@@ -5,5 +5,26 @@ app.listen(5000, (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-  res.send("server is sending you some gifts...take it .......");
+  // res.send("server is sending you some gifts...take it .......");
+  // res.redirect("/status");
+  res.sendFile(__dirname + "/views/login.html");
 });
+
+app.get("/status", (req, res) => {
+  res.status(206).json({
+    name: "shahriar",
+    sms: "nothing",
+    statusCode: 206,
+  });
+});
+
+app.get("/cookies", (req, res) => {
+  // res.cookie("shahriar", "25");
+  res.clearCookie("shahriar");
+  res.append("id", "423");
+  res.end();
+});
+
+// app.use((req, res, next) => {
+//   res.send("404 page not found.......");
+// });
