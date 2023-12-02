3cd55f4d113af5e1b18d8dc50e6b0169bdc91aa2
shahriar021 <shahriarc19@gmail.com>
2023-12-02 21:09:05 +0600

    only post method for form

--- exp_2/index.js
+++ exp_2/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+var bodyParser = require("body-parser");
+
+// create application/json parser
+app.use(bodyParser.json());
+
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //query parameter
 
 // app.get("/", (req, res) => {
@@ -16,6 +24,20 @@ app.get("/userId/:id/userAge/:age", (req, res) => {
   //   const age = req.params.age;
   const { id, age } = req.params;
   res.send(`your id is ${id} and your age is ${age}`);
+});
+
+app.post("/user", (req, res) => {
+  const name = req.body.name;
+  res.send(`your name is ${name}`);
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/register", (req, res) => {
+  const name = req.body.nickName;
+  res.send(`your name is ${name}`);
 });
 
 app.listen(5000);
