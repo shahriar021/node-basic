c725c2eca194d4f31406dec1cb200d97aebf6f64
shahriar021 <shahriarc19@gmail.com>
2023-12-03 07:50:43 +0600

    middleware 1

---
+++ exp_4/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 6001;
+
+const firstMiddleware = (req, res, next) => {
+  console.log("this is middleware");
+  console.log((req.currentTime = new Date(Date.now())));
+  next();
+};
+
+app.get("/", firstMiddleware, (req, res) => {
+  console.log("home page");
+  res.send("hellow");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started........ at http://localhost:${PORT}`);
+});
